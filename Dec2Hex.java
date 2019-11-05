2246120f8c69339a3fa3c464c586901d840fc356
dani-v <danieladvasileva@gmail.com>
2019-11-05 14:59:24 +0000

    Added an exception

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,14 +1,12 @@
 import java.util.Scanner;
 
-//Test
-
 class Dec2Hex
+
 {
 
     public static int Arg1;
 
-    public static void main(String args[])
-    {
+    public static void main(String args[])throws Exception {
         if (args.length > 0) 
         {
             try 
@@ -21,11 +19,15 @@ class Dec2Hex
                 System.exit(1);
             }
         }
+        else { 
+        	System.err.println("You must enter an integer input!");
+        	System.exit(0);      
+        }
 
     	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-         int rem, num;
-	num = Arg1;
-	String hexadecimal=""; 
+        int rem, num;
+		num = Arg1;
+		String hexadecimal=""; 
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
         
         while(num != 0)
