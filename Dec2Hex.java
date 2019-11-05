e26b96835533ec3f8000b0472cf7149128b0589b
dani-v <danieladvasileva@gmail.com>
2019-11-05 23:08:12 +0000

    Test

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,21 +1,18 @@
 // import java.util.Scanner; 
 
-class Dec2Hex
+class Dec2Hex{
 
-{
-
-    private static final int Arg;
+    private static int Arg;
 
     public static void main(String args[]) throws Exception {
 
-        if (args.length > 0) 
-        {
-            try 
-            {
+        if (args.length > 0) {
+            try{
+
                 Arg = Integer.parseInt(args[0]);
             } 
-            catch (NumberFormatException e) 
-            {
+            catch (NumberFormatException e){
+
                 System.err.println("Argument" + args[0] + " must be an integer.");
                 System.exit(1);
             }
@@ -27,7 +24,6 @@ class Dec2Hex
         	System.exit(0);      
         }
 
-
     	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem; 
         int num;
@@ -35,14 +31,12 @@ class Dec2Hex
 		String hexadecimal=""; 
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
         
-        while(num != 0)
-        {
+        while(num != 0){
 	    rem=num%16;
 	    hexadecimal= ch[rem] + hexadecimal;
             num= num/16;
         }
         
         System.out.println("Hexadecimal representation is : " + hexadecimal);
-
     }
 }
