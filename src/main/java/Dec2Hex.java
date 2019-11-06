4b650a6962c916896dc5a534518292b1156ed95d
dani-v <danieladvasileva@gmail.com>
2019-11-06 22:53:43 +0000

    Changes to code

--- src/main/java/Dec2Hex.java
+++ src/main/java/Dec2Hex.java
@@ -22,7 +22,8 @@ public class Dec2Hex{
         
         convertDec2Hex(arg);
     }
-        
+
+    //Method that converts decimal to hexadecimal    
     public static String convertDec2Hex(int decimal){
     	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem; 
